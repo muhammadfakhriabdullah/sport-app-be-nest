bbf3d761d5322fb91b7dcd7359e6f4b8afd6a4c5
muhammadfakhriabdullah <mfakhriabdullah@gmail.com>
2026-03-16 00:36:36 +0700

    feat: add profiles module with CRUD functionality and integrate into app module

---
+++ src/profiles/profiles.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Profile } from './profiles.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProfilesService {
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+  ) {}
+
+  async create(data: Partial<Profile>): Promise<Profile> {
+    return this.profileRepository.save(data);
+  }
+
+  async findAll(): Promise<Profile[]> {
+    return this.profileRepository.find();
+  }
+
+  async findOne(id: number): Promise<Profile | null> {
+    return this.profileRepository.findOneBy({ id: BigInt(id) });
+  }
+
+  async update(id: number, data: Partial<Profile>): Promise<Profile | null> {
+    await this.profileRepository.update(id, data);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.profileRepository.delete(id);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,11 @@
 // users/users.controller.ts
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
