c0a37d9d0f9cafbd2cb4a311b6a6a78bb50e0ad0
muhammadfakhriabdullah <mfakhriabdullah@gmail.com>
2026-03-21 02:33:02 +0700

    feat(users): add JWT guard and findOne method to fetch user by ID

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,11 +4,15 @@ import {
   Body,
   Controller,
   Get,
+  Param,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -40,6 +44,24 @@ export class UsersController {
     return {
       success: true,
       message: 'User created successfully',
+      data: user,
+    };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) {
+      throw new BadRequestException({
+        success: false,
+        message: 'User not found',
+      });
+    }
+
+    return {
+      success: true,
+      message: 'User fetched successfully',
       data: user,
     };
   }
